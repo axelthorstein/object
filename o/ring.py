6fe88a9065d5a833a355a85276fe795ffb0a3f54
axelsteingrimsson <axel.steingrimsson@shopify.com>
2018-04-29 22:44:00 -0400

    successfully identifying edges and colors

--- o/ring.py
+++ o/ring.py
@@ -202,7 +202,6 @@ class SimpleRing(Ring):
         Returns:
             str: The ring color.
         """
-
         return max(dict(self.color_freq["outer"]).items(), key=itemgetter(1))[0]
 
     @timecall
@@ -238,8 +237,6 @@ class SimpleRing(Ring):
         Returns:
             int: The outer edges.
         """
-
-        #  TODO: use average ring width.
         left = self.walk(SimpleRing.left(self.inner_edges["left"]),
            SimpleRing.left, depth="outer")
         up = self.walk(SimpleRing.up(self.inner_edges["up"]),
