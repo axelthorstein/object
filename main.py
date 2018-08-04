c0d553fc80d2588beb319b12931d7f5016d2bd08
Axel Thor <axelthorstein@gmail.com>
2018-08-04 17:09:41 -0400

    removing extra lines

--- main.py
+++ main.py
@@ -44,9 +44,3 @@ def items(image_id):
     db.clean_up()
 
     return redirect(GraphQL.create_checkout(product))
-
-
-if __name__ == '__main__':
-    # This is used when running locally. Gunicorn is used to run the
-    # application on Google App Engine. See entrypoint in app.yaml.
-    app.run(host='127.0.0.1', port=8080)
